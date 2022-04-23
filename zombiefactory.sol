92dadce9a05f16e991787fad0069cee06de21cce
superstar <83311833+clever-web@users.noreply.github.com>
2022-04-23 18:41:33 +0300

    lesson2-commit

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -15,10 +15,15 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) internal {
         zombies.push(Zombie(_name, _dna));
 
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -28,6 +33,7 @@ contract ZombieFactory {
     }
 
     function _createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[mas.sender] == 0);
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
