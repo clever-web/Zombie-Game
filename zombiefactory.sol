ce2950859fdd0dbf7f45a4f991df81dc99670a39
superstar <83311833+clever-web@users.noreply.github.com>
2022-04-25 07:28:49 +0300

    lesson3-commit

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,16 +1,21 @@
 
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
+import "./ownable.sol";
+
+contract ZombieFactory is Ownable {
 
     event NewZombie(uint zombieID, stirng name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
         string name;
         uint dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -19,9 +24,10 @@ contract ZombieFactory {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string memory _name, uint _dna) internal {
-        zombies.push(Zombie(_name, _dna));
+        // Consider this 
+        // zombies.push(Zombie(_name, _dna));
 
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime))) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -20,13 +20,28 @@ contract kittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    kittyInterface kittyContract;
 
-    KittyInterface kittyContract = KittyInterface(ckAddress);
+    // address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    // KittyInterface kittyContract = KittyInterface(ckAddress);
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+    function setKittyContractAddress(address _address) external onlyOwner {
+        kittyContract = kittyInterface(_address);
+    }
+
+    function _triggerCooldown(Zombie storage _zombie) internal {
+        _zombie.readyTime = uint32(now + cooldownTime);
+    }
+
+    function _isReady(Zombie storage _zombie) internal view returns (bool) {
+        return (_zombie.readyTime <= now);
+    }
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+
+        require(_isReady(myZombie));
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna + _targetDna) / 2;
 
@@ -34,6 +49,8 @@ contract ZombieFeeding is ZombieFactory {
             newDna = newDna - newDna % 100 + 99;
         }
         _createZombie("NoName", newDna);
+
+        _triggerCooldown(myZombie);
     }
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
